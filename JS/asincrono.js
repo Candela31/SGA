2f398e72328742d0c035677a9a1a0f1b9cf159b3
Candela31 <102998114+Candela31@users.noreply.github.com>
2026-08-10 14:27:49 -0300

    Actualizacion

---
+++ JS/asincrono.js
@@ -0,0 +1,37 @@
+// console.log("inicio")
+// setTimeout(() => {
+//     console.log("Buscando alumno...")
+// }, 3000)
+// console.log("fin")
+
+// function saludar(){
+//     console.log("Hola")
+// }
+// function ejecutar(funcion){
+//     funcion()
+// }
+// ejecutar(saludar)
+
+// function despedirse(){
+//     console.log("Adios")
+// }
+// setTimeout(despedirse, 3000)
+
+// setTimeout(() => {
+//     console.log("Buscando docente...")
+// }, 2000)
+
+// setTimeout(() => {
+//     console.log("Buscando materia...")
+// }, 4000)
+
+// setTimeout(() => {
+//     console.log("Buscando curso...")
+// }, 1000)
+
+console.log("Abriendo SGA...")
+setTimeout(() => {
+    console.log("Alumnos cargados")
+}, 3000)
+
+console.log("El usuario puede seguir navegando") 
